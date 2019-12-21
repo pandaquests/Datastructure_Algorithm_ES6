65ca1296706262de7d9ccd7f31fa6e171ea0056e
thadeuszlay <thadeuszlay@gmail.com>
2019-12-21 13:45:20 +0100

    chore: Delete not needed code

--- code/datastructures/Stack.js
+++ code/datastructures/Stack.js
@@ -11,65 +11,6 @@ class Stack {
 	pop() {
 		return this.store.pop();
 	}
-	// -------------------
-	clear() {
-		this.store = [];
-		this.size = this.pos = 0;
-	}
-	toString() {
-		return this.store.join(',');
-	}
-	get(index = this.pos) {
-		return this.store[index];	
-	}
-	indexOf (item) {
-		return this.store.indexOf(item);
-	}
-	lastIndexOf (item) {
-		return this.store.lastIndexOf(item);
-	}
-	isEmpty () {
-		return this.store.length === 0;		
-	}
-	insert(el, index) {
-		this.store.splice(index, 0, el);
-	}
-	add(el) {
-		this.store.push(el);
-	}
-	remove(index) {
-		this.store.splice(index, 1);
-	}
-	front() {
-		this.pos = 0;
-	}
-	end() {
-		this.pos = this.store.length - 1;
-	}
-	prev() {
-		if (this.pos === 0) {
-			return 0;
-		}
-		return --this.pos;
-	}
-	next() {
-		if (this.pos === this.store.length - 1) {
-			return this.pos;
-		}
-		return ++this.pos;
-	}
-	moveTo(index) {
-		if (index >= this.store.length) {
-			this.pos = this.store.length - 1;
-		} else if (index < 0) {
-			this.pos = 0; 
-		} else {
-			this.pos = index;
-		}	
-	}
-	sublist(from, to) {
-		return this.store.slice(from, to);
-	}
 }
 
 module.exports = Stack;
