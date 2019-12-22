c6788c6ea3ac43c7584f78478088bd3ec76497d5
pq <hi.pandaquests@gmail.com>
2019-12-22 19:05:57 +0100

    feature: Add stack description

--- code/datastructures/Stack.js
+++ code/datastructures/Stack.js
@@ -11,6 +11,19 @@ class Stack {
 	pop() {
 		return this.store.pop();
 	}
+	peek() {
+		if (!this.store.length) {
+			return;
+		}
+		return this.store[this.store.length - 1];
+	}
+	search(el) {
+		for (let i = this.store.length - 1; i >= 0; i--) {
+			if (this.store[i] === el) {
+				return this.store.length - i;
+			}
+		}
+	}
 }
 
 module.exports = Stack;
--- code/test/datastructures/Stack.test.js
+++ code/test/datastructures/Stack.test.js
@@ -1,4 +1,3 @@
-const List = require('../../datastructures/List');
 const Stack = require('../../datastructures/Stack');
 
 test('Test init empty array', () => {
@@ -15,98 +14,21 @@ test('Test push and pop an element', () => {
 	expect(myStack.pop()).toBe(num);
 	expect(myStack.empty()).toBe(true);
 });
-// =======================
-test('Test init empty array', () => {
-  const myList = new List();
-  expect(myList.store.length).toBe(0);
-  expect(myList.pos).toBe(0);
-  expect(myList.store).toStrictEqual([]);
+test('Test peek', () => {
+	const myStack = new Stack();
+	expect(myStack.empty()).toBe(true);
+	expect(myStack.peek()).toBe(undefined);
+	const num = 2;
+	myStack.push(num);
+	expect(myStack.empty()).toBe(false);
+	expect(myStack.peek()).toBe(num);
 });
+test('Test search', () => {
+	const myStack = new Stack();
+	const nums = [1,2,3,2,0,4];
+	nums.forEach(n => myStack.push(n));
 
-test('Test init filled array', () => {
-  const myList = new List([1,2,3,4]);
-  expect(myList.store.length).toBe(4);
-  expect(myList.pos).toBe(0);
-  expect(myList.store).toStrictEqual([1,2,3,4]);
-});
-
-test('clear array', () => {
-  const myList = new List([1,2,3,4]);
-  expect(myList.store).toStrictEqual([1,2,3,4]);
-  myList.clear();
-  expect(myList.store).toStrictEqual([]);
-});
-
-test('to string', () => {
-  const myList = new List([1,2,3,4]);
-  expect(myList.toString()).toStrictEqual('1,2,3,4');
-});
-
-test('get item', () => {
-  const myList = new List([1,2,3,4]);
-  expect(myList.get(1)).toBe(2);
-  expect(myList.get()).toBe(1);
-});
-
-test('index of ', () => {
-  const myList = new List([1,2,3,4,3]);
-  expect(myList.indexOf(3)).toBe(2);
-});
-
-test('last index of ', () => {
-  const myList = new List([1,2,3,4,3]);
-  expect(myList.lastIndexOf(3)).toBe(4);
-});
-
-test('is empty ', () => {
-  const myList = new List();
-  expect(myList.isEmpty()).toBeTruthy();
-});
-
-test('is empty ', () => {
-  const myList = new List([1,2,3,4]);
-  myList.insert(99, 1);
-  expect(myList.store).toStrictEqual([1,99,2,3,4]);
-});
-
-test('append item ', () => {
-  const myList = new List([1,2,3,4]);
-  myList.add(99);
-  expect(myList.lastIndexOf(99)).toBe(myList.store.length - 1);
-});
-
-test('remove item ', () => {
-  const myList = new List([1,2,3,4]);
-  expect(myList.store.length).toBe(4);
-  myList.remove(0);
-  expect(myList.store.length).toBe(3);
-});
-
-test('set pos to front ', () => {
-  const myList = new List([1,2,3,4]);
-  expect(myList.pos).toBe(0);
-  expect(myList.next()).toBe(1);
-  expect(myList.front());
-  expect(myList.pos).toBe(0);
-  expect(myList.end());
-  expect(myList.pos).toBe(myList.store.length - 1);
-  expect(myList.prev());
-  expect(myList.pos).toBe(myList.store.length - 2);
-  expect(myList.moveTo(3));
-  expect(myList.pos).toBe(3);
-});
-
-test('set pos to front ', () => {
-  const myList = new List([1,2,3,4]);
-  expect(myList.moveTo(99));
-  expect(myList.pos).toBe(3);
-  expect(myList.moveTo(-99));
-  expect(myList.pos).toBe(0);
-  expect(myList.moveTo(2));
-  expect(myList.pos).toBe(2);
-});
-
-test('sublist ', () => {
-  const myList = new List([1,2,3,4]);
-  expect(myList.sublist(1,3)).toStrictEqual([2,3]);
+	expect(myStack.search(4)).toBe(1);
+	expect(myStack.search(2)).toBe(3);
+	expect(myStack.search(5)).toBe(undefined);
 });
